9e8d5424070b49a8acd4a3f09089134cf4a2487a
mymany <p675039s@mail.kyutech.jp>
2019-10-26 15:56:13 +0900

    [update] user create apiの実装

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,21 @@ module Api
                 @users = User.all
                 render json: @users
             end
+
+            def create
+                @user = User.new(user_params)
+                @user.save
+                render json: @user
+            end
+
+            private
+            def user_params
+              params.require(:user).permit(:email)
+            end
+
+            def set_user
+            #   params.require(:user).permit(:email)
+            end
         end
     end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
     it 'get_users' do
         @user = FactoryBot.create(:user)
         get "/api/v1/users"
@@ -11,5 +10,15 @@ RSpec.describe "Users", type: :request do
         expect(@subject_user["id"]).to be_truthy
         expect(@subject_user["email"]).to be_truthy
         @user.delete
+    end
+
+    it 'post_user' do
+        req_params = { email: 'dummy_user@example.com' }
+
+        expect { post "/api/v1/users", params: { user: req_params } }.to change(User, :count).by(+1)
+
+        @subject_user = JSON.parse(response.body)
+        expect(@subject_user["id"]).to be_truthy
+        expect(@subject_user["email"]).to be_truthy
     end
 end
