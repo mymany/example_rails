3a46eabf64e390b6e84fae62f9b552f233cb1fd9
mymany <p675039s@mail.kyutech.jp>
2019-10-26 17:04:36 +0900

    [update] userのテストコードにdescribeでまとめる

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,28 +2,32 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-    it 'get_users' do
-        @user = FactoryBot.create(:user)
-        get "/api/v1/users"
-        @json = JSON.parse(response.body)
-        @subject_user = @json.first
+    describe "GET /users" do
+        it 'get_users' do
+            @user = FactoryBot.create(:user)
+            get "/api/v1/users"
+            @json = JSON.parse(response.body)
+            @subject_user = @json.first
 
-        expect(@subject_user["id"]).to be_truthy
-        expect(@subject_user["email"]).to be_truthy
-        @user.delete
+            expect(@subject_user["id"]).to be_truthy
+            expect(@subject_user["email"]).to be_truthy
+            @user.delete
+        end
     end
 
-    it 'post_user' do
-        req_params = {
-            email: 'dummy_user@example.com',
-            password: 'password'
-        }
+    describe "POST /users" do
+        it 'post_user' do
+            req_params = {
+                email: 'dummy_user@example.com',
+                password: 'password'
+            }
 
-        expect { post "/api/v1/users", params: { user: req_params } }.to change(User, :count).by(+1)
+            expect { post "/api/v1/users", params: { user: req_params } }.to change(User, :count).by(+1)
 
-        @subject_user = JSON.parse(response.body)
-        expect(@subject_user["id"]).to be_truthy
-        expect(@subject_user["email"]).to be_truthy
-        expect(@subject_user["point"]).to eq User::INITIAL_POINT
+            @subject_user = JSON.parse(response.body)
+            expect(@subject_user["id"]).to be_truthy
+            expect(@subject_user["email"]).to be_truthy
+            expect(@subject_user["point"]).to eq User::INITIAL_POINT
+        end
     end
 end
