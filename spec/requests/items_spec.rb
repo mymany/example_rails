fc6e13f688840da7f820cd5910d25726fc11c14c
mymany <p675039s@mail.kyutech.jp>
2019-10-27 19:52:15 +0900

    [update] テストコードのリファクタリング
    - describe, context, it, subjectでテストケースを整理

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe "Items", type: :request do
         after do
             @item.delete
         end
-        context "ユーザが存在するとき" do
-            it "期待するキーが存在すること" do
+        context "すでに商品が存在するとき" do
+            it "レスポンスに期待するキーが存在すること" do
                 subject
                 json = JSON.parse(response.body).first
                 expect(json["id"]).to be_truthy
@@ -24,35 +24,46 @@ RSpec.describe "Items", type: :request do
                 expect(response).to have_http_status 200
             end
         end
+        context "商品が存在しないとき" do
+        end
     end
 
     describe "POST /items", type: :post do
         subject { post "/api/v1/items", params: { item: req_params }}
-        context "商品登録処理が成功したとき" do
+        context "期待するパラメータでリクエストされたとき" do
             let(:user) { FactoryBot.create(:user) }
             let(:req_params) { {user_id: user.id, name: Faker::Name.initials, point: 100 } }
             it { expect{subject}.to change{ Item.count }.by(1) }
-            it "期待するキーが存在すること" do
+            it "レスポンスに期待するキーが存在すること" do
                 subject
                 json = JSON.parse(response.body)
                 expect(json["id"]).to be_truthy
-                expect(json["name"]).to eq req_params[:name]
-                expect(json["point"]).to eq req_params[:point]
-                expect(json["user_id"]).to eq req_params[:user_id]
+                expect(json["name"]).to be_truthy
+                expect(json["point"]).to be_truthy
+                expect(json["user_id"]).to be_truthy
             end
             it "ステータスコードが201であること" do
                 subject
                 expect(response).to have_http_status 201
             end
         end
+        context "期待するパラメータでリクエストされなかったとき" do
+            let(:user) { FactoryBot.create(:user) }
+            let(:req_params) { {user_id: -1, name: Faker::Name.initials, point: 100 } }
+            it { expect{subject}.to change{ Item.count }.by(0) }
+            it "ステータスコードが422であること" do
+                subject
+                expect(response).to have_http_status 422
+            end
+        end
     end
 
     describe "PUT /items", type: :put do
         subject { put "/api/v1/items/" + item.id.to_s, params: { item: req_params }}
-        context "商品更新処理が成功したとき" do
+        context "期待するパラメータでリクエストされたとき" do
             let(:item) { FactoryBot.create(:item) }
             let(:req_params) { {user_id: item.user.id, name: Faker::Name.initials, point: 100 } }
-            it "期待するキーが存在すること" do
+            it "レスポンスに期待するキーが存在すること" do
                 subject
                 json = JSON.parse(response.body)
                 expect(json["id"]).to be_truthy
@@ -72,6 +83,14 @@ RSpec.describe "Items", type: :request do
                 expect(response).to have_http_status 200
             end
         end
+        context "期待するパラメータでリクエストされなかったとき" do
+            let(:item) { FactoryBot.create(:item) }
+            let(:req_params) { {user_id: -1, name: Faker::Name.initials, point: 100 } }
+            it "ステータスコードが422であること" do
+                subject
+                expect(response).to have_http_status 422
+            end
+        end
     end
 
     describe "DELETE /items", type: :delete do
@@ -79,12 +98,20 @@ RSpec.describe "Items", type: :request do
             @item = FactoryBot.create(:item)
         end
         subject { delete "/api/v1/items/" + item.id.to_s }
-        context "商品削除処理が成功したとき" do
+        context "期待するパラメータでリクエストされたとき" do
             let(:item) {@item}
             it { expect{subject}.to change{ Item.count }.by(-1) }
             it "ステータスコードが200であること" do
                 subject
                 expect(response).to have_http_status 200
+            end
+        end
+        context "期待するパラメータでリクエストされなかったとき" do
+            let(:item) {@item}
+            it "ステータスコードが404であること" do
+                item.id = -1
+                subject
+                expect(response).to have_http_status 404
             end
         end
     end
